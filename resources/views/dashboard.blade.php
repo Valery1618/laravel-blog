f29dfcff20e366025bd17f11628614bfb3d940e2
valerie <valery.barber@gmail.com>
2024-11-04 16:50:29 +0200

    form for news data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,35 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+
+    <form method="POST" action="{{ route('dashboard') }}">
+        @csrf
+
+        <!-- Title Address -->
+        <div>
+            <x-input-label for="title" :value="__('Title')" />
+            <x-text-input id="title" class="block mt-1 w-full" type="title" name="title" :value="old('title')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('title')" class="mt-2" />
+        </div>
+
+        <!-- Content -->
+        <div class="mt-4">
+            <x-input-label for="content" :value="__('Content')" />
+
+            <x-text-input id="content" class="block mt-1 w-full"
+                          type="content"
+                          name="content"
+                          required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button class="ms-3">
+                {{ __('Publish') }}
+            </x-primary-button>
+
+   <!-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -13,5 +41,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </x-app-layout>
